fd63ff66ee44544b7ed50422bcc1fe884393f4cd
Oleg <Oleg.Bashkirov@south.rt.ru>
2022-06-20 22:35:48 +0300

    Файлы разнесены по папкам Процедуры калькуляции вызываются из одного метода Добавлено неявное ожидание

--- conftest.py
+++ conftest.py
@@ -3,7 +3,8 @@ from selenium import webdriver
 
 @pytest.fixture(scope="session")
 def browser():
-    driver = webdriver.Chrome(executable_path="./chromedriver")
+    driver = webdriver.Chrome()
     driver.maximize_window()
+    driver.implicitly_wait(5)
     yield driver
     driver.quit()
